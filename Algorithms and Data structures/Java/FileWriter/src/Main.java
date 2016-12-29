c23b3b0a2e03f72d39b63b814b16df27a8e6441d
Heath Baron-Morgan <business.heath@gmail.com>
2016-12-29 17:46:30 +0000

    Added the fileWriter project

---
+++ Algorithms and Data structures/Java/FileWriter/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+// main database directory
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		
+		System.out.println("Please enter password: ");
+		
+		Meths.password();
+	}
+}
